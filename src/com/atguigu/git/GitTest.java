802fb5f58a5ef34c257d5beeab34c3b906148760
zh0u9527 <92257130+zh0u9527@users.noreply.github.com>
2022-10-08 22:42:11 +0800

    pull test

--- src/com/atguigu/git/GitTest.java
+++ src/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("master test!");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        // 测试pull拉取操作！！！
     }
 }
